2ebdbfe44439173bde5c947005eb22229f95e7fc
Benoit Letondor <benoit.letondor@gmail.com>
2015-10-11 21:42:55 +0200

    Rename files according to dart convention

--- lib/levelup.dart
+++ lib/levelup.dart
@@ -8,19 +8,19 @@ import 'package:box2d/box2d.dart';
 import 'package:box2d/box2d_browser.dart';
 import 'package:pixi2dart/pixi2dart.dart' as PIXI;
 
-part 'src/RenderingManager.dart';
-part 'src/GameStage.dart';
-part 'src/StageContactListener.dart';
-part 'src/KeyListener.dart';
-part 'src/helper/MathHelper.dart';
-part 'src/DragNDropManager.dart';
+part 'src/rendering_manager.dart';
+part 'src/game_stage.dart';
+part 'src/stage_contact_listener.dart';
+part 'src/key_listener.dart';
+part 'src/helper/math_helper.dart';
+part 'src/drag_n_drop_manager.dart';
 
-part 'src/renderer/PhysicsItem.dart';
-part 'src/renderer/Renderer.dart';
-part 'src/renderer/Item.dart';
-part 'src/renderer/pixi/PixiItem.dart';
-part 'src/renderer/pixi/PixiPhysicsItem.dart';
-part 'src/renderer/pixi/PixiRenderer.dart';
+part 'src/renderer/physics_item.dart';
+part 'src/renderer/renderer.dart';
+part 'src/renderer/item.dart';
+part 'src/renderer/pixi/pixi_item.dart';
+part 'src/renderer/pixi/pixi_physics_item.dart';
+part 'src/renderer/pixi/pixi_renderer.dart';
 
 // Expose keylistener singleton
 KeyListener _keyListener;
